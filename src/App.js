9073413397299189ed97307087db1ef8812721ed
Ashish Lal <ashishlal2003@gmail.com>
2023-05-29 20:47:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState }from 'react'
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#212529';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
     <>
       
-    <Navbar title="TextUtils" aboutText="About Us"/>
+    <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
+    {/* <About/> */}
     <div className="container my-3">
-      <TextForm heading="Enter the text to analyze below"/>
+      <TextForm heading="Enter the text to analyze below" mode={mode}/>
     </div>
     
     </>
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -20,24 +20,52 @@ export default function TextForm(props) {
     const handleOnChange = (event)=>{
         setText(event.target.value);
     }
+
+    const handleRandom = ()=>{
+        let newText = '';
+        let str = text;
+        for(let i=0;i<str.length;i++){
+            newText += str.charAt(Math.floor(Math.random()*str.length));
+        }
+        setText(newText);
+    }
+
+    const handleCopy = ()=>{
+        let text = document.getElementById('myBox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = ()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+
     const [text, setText] = useState('');
     return (
         <>
         <div className='container beech'>
             <div className="mb-3">
-                <label htmlFor="myBox" className="form-label"><h1>{props.heading}</h1></label>
-                <textarea className="form-control" id="myBox" rows="8" value={text} placeholder='Enter text here' onChange={handleOnChange}></textarea>
+                <label htmlFor="myBox" className="form-label"><h1 style={{color:props.mode==='light'?'#212529':'white'}}>{props.heading}</h1></label>
+                <textarea className="form-control" id="myBox" rows="8" value={text} style={{
+  backgroundColor: props.mode === 'light' ? 'white' : '#212529',
+  color: props.mode === 'light' ? '#212529' : 'white'
+}}
+ placeholder='Enter text here' onChange={handleOnChange}></textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
             <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleRandom}>Randomise Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleExtraSpaces} style={{color:props.mode==='light'?'#212529':'white'}}>Remove Extra Spaces</button>
         </div>
         <div className="container my-4">
-            <h2>Your text summary</h2>
-            <p>{text.split(" ").length} words, {text.length} characters.</p>
-            <p>{0.008 * text.split(" ").length} Minutes read</p>
-            <h2>Preview</h2>
-            <p>{text}</p>
+            <h2 style={{color:props.mode==='light'?'#212529':'white'}}>Your text summary</h2>
+            <p style={{color:props.mode==='light'?'#212529':'white'}}>{text.split(" ").length} words, {text.length} characters.</p>
+            <p style={{color:props.mode==='light'?'#212529':'white'}}>{0.008 * text.split(" ").length} Minutes read</p>
+            <h2 style={{color:props.mode==='light'?'#212529':'white'}}>Preview</h2>
+            <p style={{color:props.mode==='light'?'#212529':'white'}}>{text.length>0?text:"Nothing to preview!"}</p>
         </div>
         </>
     )
