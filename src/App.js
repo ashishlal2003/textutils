aa9313c51a0d0f7a16add999c7ed335165e6fada
Ashish Lal <ashishlal2003@gmail.com>
2023-05-30 12:41:43 +0530

    final

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
-import React, { useState }from 'react'
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
-
   const [mode, setMode] = useState('light');
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#212529';
-    }
-    else{
+    } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
     }
@@ -22,13 +21,15 @@ function App() {
 
   return (
     <>
-      
-    <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
-    {/* <About/> */}
-    <div className="container my-3">
-      <TextForm heading="Enter the text to analyze below" mode={mode}/>
-    </div>
-    
+      {/* <BrowserRouter> */}
+        <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} />} />
+          </Routes>
+        </div>
+      {/* </BrowserRouter> */}
     </>
   );
 }
